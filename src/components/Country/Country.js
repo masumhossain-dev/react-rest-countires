a8dc7942f776e2b2865890d080962845fad94b85
Masum Hossain <masumhossain.dev@gmail.com>
2023-05-01 11:36:07 +0600

    apply destructuring methood

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+  const {name, area, population, region, flags, timezones} = props.country;
   return (
     <div className='countries'>
-      <h1>Name: {props.country.name.common}</h1>
-      <h3>Population: {props.country.population}</h3>
-      <p>Region: {props.country.region}</p>
-      <p>Timezone: {props.country.timezones[0]}</p>
-      <img src={props.country.flags.png}></img>
+      <h1>Country Name: {name.common}</h1>
+      <h3>Region: {region}</h3>
+      <p>Area: {area}</p>
+      <p>Population: {population}</p>
+      <p>Timestamps: {timezones[0]}</p>
+      <img src={flags.png}></img>
     </div>
   );
 };
